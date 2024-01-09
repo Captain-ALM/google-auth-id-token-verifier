3f0685a7151dde02fd78e8f1661669dee907a909
Captain ALM <captainalm@captainalm.com>
2024-01-09 15:25:44 +0000

    Update verifier.

--- verifier.go
+++ verifier.go
@@ -1,6 +1,9 @@
 package googleAuthIDTokenVerifier
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 var (
 	// ClockSkew - five minutes
@@ -13,10 +16,44 @@ var (
 	}
 )
 
-type Verifier struct{}
+func NewVerifier() *Verifier {
+	return &Verifier{
+		ClockSkew: ClockSkew,
+		Issuers:   Issuers,
+	}
+}
+
+type Verifier struct {
+	ClockSkew time.Duration
+	Issuers   []string
+}
 
 func (v *Verifier) VerifyIDToken(idToken string, audience []string) error {
 	_, err := v.ClaimIDToken(idToken, audience)
+	return err
+}
+
+func (v *Verifier) ValidateIDTokenEmail(idToken string, audience []string, email string) error {
+	clms, err := v.ClaimIDToken(idToken, audience)
+	if err == nil {
+		if clms.Email == email {
+			return nil
+		} else {
+			return errors.New("email not matching")
+		}
+	}
+	return err
+}
+
+func (v *Verifier) ValidateIDTokenSubject(idToken string, audience []string, subject string) error {
+	clms, err := v.ClaimIDToken(idToken, audience)
+	if err == nil {
+		if clms.Subject == subject {
+			return nil
+		} else {
+			return errors.New("subject not matching")
+		}
+	}
 	return err
 }
 
